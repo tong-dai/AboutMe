35c1f36c7eb96778d8831821708f5b74263ff817
tong-dai <td2471@princeton.edu>
2025-04-12 03:34:34 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Tong Dai on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMeTests/AboutMeTests.swift
@@ -0,0 +1,16 @@
+//
+//  AboutMeTests.swift
+//  AboutMeTests
+//
+//  Created by Tong Dai on 4/12/25.
+//
+
+import Testing
+
+struct AboutMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
